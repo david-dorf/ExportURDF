33ad960e1e099838c5fafe879ce494073a3f6321
daviddorf2023 <daviddorf2023@u.northwestern.edu>
2024-04-07 13:22:21 -0400

    Add folder openers to the URDF export script

--- ExportURDF.py
+++ ExportURDF.py
@@ -9,6 +9,14 @@ def run(context):
     try:
         app = adsk.core.Application.get()
         ui = app.userInterface
+        design = adsk.fusion.Design.cast(app.activeProduct)
+        rootComp = design.rootComponent
+        allComps = rootComp.allComponents
+        robotName = rootComp.name.replace(' ', '_')
+        folderOpener = ui.createFolderDialog()
+        folderOpener.title = 'Select folder to save URDF'
+        folderOpener.showDialog()
+
         ui.messageBox('URDF successfully exported!')
 
     except:
