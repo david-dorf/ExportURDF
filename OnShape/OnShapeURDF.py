69085c59b3887e4d95f6cd8355e0dfa2373b4403
daviddorf2023 <daviddorf2023@u.northwestern.edu>
2024-04-24 18:02:04 -0400

    Add mass, inertia, centroid calcs and outputs filled template to URDF. Needs to use partID for joint parent/child links.

--- OnShape/OnShapeURDF.py
+++ OnShape/OnShapeURDF.py
@@ -15,6 +15,9 @@ class OnShapeURDF:
             self.onshapeURL)
         self.assembly = self.client.get_assembly(
             self.documentID, self.workspaceID, self.elementID)
+        self.urdfFile = None
+        self.robotName = None
+        self.folderPath = None
         os.system('clear')  # Clear the terminal
 
     def createURDF(self):
@@ -23,18 +26,20 @@ class OnShapeURDF:
         robotHeader = """<robot name = "%s">\n"""
         robotFooter = """\n</robot>"""
         try:
-            robotName, folderPath = self.getFolder()
-            urdfFile = open(os.path.join(
-                folderPath, robotName, 'urdf', robotName + '.urdf'), 'w')
-            urdfFile.write(xmlHeader)
-            urdfFile.write(robotHeader % robotName)
-            linkData = self.extractLinks(folderPath, robotName)
+            self.robotName, self.folderPath = self.getFolder()
+            self.urdfFile = open(os.path.join(
+                self.folderPath, self.robotName, 'urdf', self.robotName + '.urdf'), 'w')
+            self.urdfFile.write(xmlHeader)
+            self.urdfFile.write(robotHeader % self.robotName)
+            linkData = self.extractLinks(self.folderPath, self.robotName)
             jointData = self.extractJoints()
-            urdfFile.write(robotFooter)
+            self.fillLinkTemplate(linkData)
+            self.fillJointTemplate(jointData)
+            self.urdfFile.write(robotFooter)
         except:
             print('Failed:\n{}'.format(traceback.format_exc()))
 
-    @staticmethod
+    @ staticmethod
     def getFolder():
         """Get the folder path to save the URDF and mesh files."""
         robotName = input("\nEnter the name of the robot: ")
@@ -64,24 +69,38 @@ class OnShapeURDF:
         elementID = url.split('e/')[1]
         return documentID, workspaceID, elementID
 
-    def extractLinks(self, folderPath: str, robotName: str) -> list[str]:
+    def extractLinks(self, folderPath: str, robotName: str) -> list[dict]:
         """Extract the link information from the assembly and save the meshes as STL files."""
-        partNameList = []
+        linkDataSet = []
         for instance in self.assembly["rootAssembly"]["instances"]:
+            linkData = {}
             if instance["type"] == "Part":
                 partName = self.formatName(instance["name"])
-                partNameList.append(partName)
                 stl = self.client.part_studio_stl_m(self.documentID,
                                                     instance["documentMicroversion"],
                                                     instance["elementId"], instance["partId"])
-                with open(os.path.join(folderPath, robotName, 'meshes', partName + '.stl'), 'wb') as f:
+                with open(os.path.join(folderPath, robotName,
+                                       'meshes', partName + '.stl'), 'wb') as f:
                     f.write(stl)
-
+                massProperties = self.client.part_mass_properties(
+                    self.documentID, instance["documentMicroversion"],
+                    instance["elementId"], instance["partId"])
+                mass = massProperties["bodies"][instance["partId"]]["mass"]
+                inertia = massProperties["bodies"][instance["partId"]]["inertia"]
+                centroid = massProperties["bodies"][instance["partId"]]["centroid"]
+                linkData = {
+                    "name": partName,
+                    "origin": centroid,
+                    "meshPath": os.path.join('meshes', partName + '.stl'),
+                    "mass": mass,
+                    "inertia": inertia
+                }
+                linkDataSet.append(linkData)
             elif instance["type"] == "Assembly":
                 print("Sub-assembly found, not supported yet")
             else:
                 print("Unknown instance type: " + instance["type"])
-        return partNameList
+        return linkDataSet
 
     def extractJoints(self) -> list[dict]:
         """Extract the joint information from the assembly."""
@@ -96,6 +115,7 @@ class OnShapeURDF:
                     "child": feature["featureData"]["matedEntities"][1]["matedOccurrence"],
                     "rotationAxis": self.getRotationAxis(matedCS),
                     "origin": matedCS["origin"],
+                    "limit": None  # TODO: Add limit for revolute and prismatic joints
                 }
                 joints.append(joint)
         return joints
@@ -115,21 +135,60 @@ class OnShapeURDF:
         else:
             return name.translate(str.maketrans(' :()<>', '______'))
 
-    def fillLinkTemplate(self, linkName: str, origin: np.array,
-                         meshPath: str, mass: float, inertia: np.array) -> str:
+    def fillLinkTemplate(self, linkDataSet: list[dict]):
         """Fill the link template with the given values."""
-        return self.getTemplate('link') % (linkName, origin[0], origin[1], origin[2], meshPath,
-                                           mass, inertia[0], inertia[1], inertia[2], inertia[3],
-                                           inertia[4], inertia[5], inertia[6])
+        for linkData in linkDataSet:
+            linkName = linkData["name"]
+            origin = linkData["origin"]
+            meshPath = linkData["meshPath"]
+            mass = linkData["mass"][0]
+            inertia = linkData["inertia"]
+            self.urdfFile.write(self.getTemplate('link') % (linkName, origin[0], origin[1],
+                                                            origin[2], self.robotName, meshPath,
+                                                            origin[0], origin[1], origin[2],
+                                                            self.robotName, meshPath, origin[0],
+                                                            origin[1], origin[2], mass, inertia[0],
+                                                            inertia[1], inertia[2], inertia[3],
+                                                            inertia[4], inertia[5]))
 
-    def fillJointTemplate(self, jointName: str, jointType: str, origin: np.array,
-                          parentLink: str, childLink: str, axis: np.array, limits: np.array) -> str:
+    def fillJointTemplate(self, jointData: list[dict]):
         """Fill the joint template with the given values."""
-        return self.getTemplate(jointType) % (jointName, origin[0], origin[1], origin[2],
-                                              parentLink, childLink, axis[0], axis[1], axis[2],
-                                              limits[0], limits[1])
+        for joint in jointData:
+            jointName = joint["name"]
+            jointType = joint["type"].lower()
+            print(jointType)
+            origin = joint["origin"]
+            parent = joint["parent"]
+            child = joint["child"]
+            rotationAxis = joint["rotationAxis"]
+            if jointType == "fastened":
+                self.urdfFile.write(self.getTemplate(jointType) % (jointName, origin[0],
+                                                                   origin[1], origin[2],
+                                                                   parent, child))
+            elif jointType == "revolute":
+                limit = joint["limit"]
+                if limit:
+                    self.urdfFile.write(self.getTemplate(jointType) %
+                                        (jointName, origin[0], origin[1], origin[2], parent, child,
+                                         rotationAxis[0], rotationAxis[1], rotationAxis[2], limit[0],
+                                         limit[1]))
+                else:
+                    self.urdfFile.write(self.getTemplate("continuous") % (jointName, origin[0],
+                                                                          origin[1], origin[2],
+                                                                          parent, child,
+                                                                          rotationAxis[0],
+                                                                          rotationAxis[1],
+                                                                          rotationAxis[2]))
+            elif jointType == "slider":
+                limit = joint["limit"]
+                self.urdfFile.write(self.getTemplate(jointType) % (jointName, origin[0], origin[1],
+                                                                   origin[2], parent, child,
+                                                                   rotationAxis[0], rotationAxis[1],
+                                                                   rotationAxis[2], limit[0], limit[1]))
+            else:
+                raise ValueError("Unknown joint type: " + jointType)
 
-    def getTemplate(templateName: str) -> str:
+    def getTemplate(self, templateName: str) -> str:
         """Get the URDF XML template based on the joint or link name."""
         LINK = """
         <link name = "%s">
@@ -160,7 +219,7 @@ class OnShapeURDF:
             <axis xyz = "%f %f %f"/>
         </joint>
         """
-        FIXED = """
+        FASTENED = """
         <joint name = "%s" type = "fixed">
             <origin xyz = "%f %f %f" rpy = "0 0 0"/>
             <parent link = "%s"/>
@@ -176,7 +235,7 @@ class OnShapeURDF:
             <limit lower = "%f" upper = "%f"/>
         </joint>
         """
-        PRISMATIC = """
+        SLIDER = """
         <joint name = "%s" type = "prismatic">
             <origin xyz = "%f %f %f" rpy = "0 0 0"/>
             <parent link = "%s"/>
